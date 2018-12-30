3b719dead9a7ab414f1dbedfe2b9b33016ae6d4a
Valentin Hilbig <valentin.hilbig@sol-venetus.de>
2018-12-30 21:13:06 +0000

    Allow additional info
    (to group caching of different authorized_keys)

--- sshlogin.sh
+++ sshlogin.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #
 # Put this in ~/.ssh/authorized_keys like
-#	command=".../sshlogin.sh 3600",no-X11-forwarding KEY description
+#	command=".../sshlogin.sh 3600 [INFO]",no-X11-forwarding KEY description
 # the number is the seconds to cache an authentication by the given IP.
-# Default is 3600
+# Default is 3600, use 0 for no caching
+# Caching is done by IP.  Use INFO to cache separated keys from the same IP
 
 AUTHFILE=.ssh/sshlogin.tmp
 
@@ -11,23 +12,24 @@ checkauth()
 {
 NOW="$(date +%s)"
 IP="${SSH_CONNECTION%% *}"
-have=false
 [ -f "$AUTHFILE" ] &&
-while read -r stamp ip
+{
+have=false
+while read -r stamp ip more
 do
 	let diff=NOW-stamp
 	[ $diff -lt "$1" ] || continue
 	have=:
-	[ ".$ip" = ".$IP" ] && return 0
+	[ ".$ip $more" = ".$IP $2" ] && return 0
 done < "$AUTHFILE"
-
 $have || rm -f "$AUTHFILE"
+}
 
-read -rp 'auth: ' auth || return 1
+read -rp "$(date +%Y%m%d-%H%M%S) auth: " auth || return 1
 google-auth "$auth" || return 1
 
-echo "$NOW $IP" >> "$AUTHFILE"
+echo "$NOW $IP $2" >> "$AUTHFILE"
 return 0
 }
 
-checkauth "${1:-3600}" && exec -- ${SSH_ORIGINAL_COMMAND:-"$SHELL"}
+checkauth "${1:-3600}" "${*:2}" && exec -- ${SSH_ORIGINAL_COMMAND:-"$SHELL" -l}
