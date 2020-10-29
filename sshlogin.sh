f206eca13b5c19f1fe69f68be396863c539111ef
Valentin Hilbig <webmaster@scylla-charybdis.com>
2020-10-29 04:46:18 +0100

    Also print timezone
    This adresses issue #2 a bit.
    
    One of my machine was way off the time normal,
    and also was sete to UTC (which is fine to me),
    but this was not reflected on the timestamp of the login,
    so it was a bit tricky to synchronize the phone
    to the correct time to get a proper TOTP from it.

--- sshlogin.sh
+++ sshlogin.sh
@@ -29,7 +29,7 @@ done < "$AUTHFILE"
 $have || rm -f "$AUTHFILE"
 }
 
-read -rp "$(id -un)@$(uname -n) $(date +%Y%m%d-%H%M%S)${info:+ }$info auth: " auth || return 1
+read -rp "$(id -un)@$(uname -n) $(date +%Y%m%d-%H%M%S%z)${info:+ }$info auth: " auth || return 1
 oath-auth "$auth" || return 1
 
 echo "$NOW $IP $info" >> "$AUTHFILE"
